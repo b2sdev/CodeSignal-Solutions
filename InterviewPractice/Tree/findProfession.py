7c7cd260e18e9415f47d69249bb67c78f48cbff0
golbanghacker <snowman.won@gmail.com>
2024-06-12 19:09:41 +0900

    Remove Tree

--- InterviewPractice/Tree/findProfession.py
+++
@@ -1,6 +0,0 @@
-def solution(level, pos):
-    if level == 1:
-        return "Engineer"
-    if solution(level - 1, (pos + 1) // 2) == "Doctor":
-        return "Doctor" if pos % 2 else "Engineer"
-    return "Engineer" if pos % 2 else "Doctor"
